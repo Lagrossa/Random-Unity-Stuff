4ba28ef51c55e996f68370d925434bcd80d131cd
Yannis Lagrosa <65159359+Lagrossa@users.noreply.github.com>
2022-03-01 07:54:27 -0600

    Plane Movement Controller
    I made a plane model and a controller for the plane using new math that I learned about vectors, lerps, and rays. This was a bit of a dive into the deep end since I also researched a bit into Quaternions (which I still don't understand). Documenting this just in case I forget...
    
    A ray is cast from the mouse position on the camera to the "world". The position of the camera is added to the endpoint of the line so that it can properly visualize the direction that the plane will travel in, proportionate to the local vector. I had to translate the plane according to the local space because using world space ended up breaking the direction. I multiplied by Time.DeltaTime to normalize the speed (Although it might move faster with a higher FPS computer... I'm not sure how that's fixed.) and then I also added a speed multiplier to that normalization because my computer is kinda slow and the plane wasn't moving fast enough for my liking. The Inverse Lerp and Quaternion bit towards the end manages the rotation of the plane. I used Inverse Lerp to get the percent of the screen that the mouse is taking up ( I'm pretty sure this would only work for my computer, I don't know how it would work on a larger monitor because this is based on pixel count... And obviously, a larger monitor has more pixels. I needed this as a percentage specifically so that I could have a modifiable and easy way to interpolate between different values. This decimal would be my "t" value in the Lerp formula. By plugging that value into another lerp function that scales the rotation of the plane's x-axis between -20 and 20, I'm able to rotate the plane by 20 degrees when the mouse is all the way to the right of the screen and to rotate it -20 degrees when the mouse is all the way to the left.

---
+++ MovementController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MovementController : MonoBehaviour
+{
+    public Transform acPlane;
+    public int rayDist = 1;
+    public Transform plane;
+    public Transform cammera;
+    [Range(0,5)]
+    public int scalar;
+    public Vector3 position;
+    public float positionMagnitude;
+    public float radius;
+    public float deLimiter = 2f;
+    public int speedMultiplier = 2;
+    public Vector3 pixel;
+    public float pixelLerp;
+
+
+    [Range(-20, 20)]
+    public float xRotation;
+
+    //Object Defs
+
+
+    private void Start()
+    {
+
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Ray rayz = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 planeVec = plane.position;
+        Vector3 cameraVec = cammera.position;
+        positionMagnitude = position.magnitude;
+        Vector3 posNorm = position.normalized;
+        radius = positionMagnitude;
+        Vector3 distanceFromCamera = planeVec - cameraVec;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(cameraVec, radius);
+        Gizmos.color = posNorm.magnitude * scalar >= radius ? Color.red : Color.green;
+        Gizmos.DrawLine(cameraVec, position);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(cameraVec, rayz.direction + cameraVec);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(cameraVec, -rayz.direction + cameraVec);
+        Gizmos.DrawSphere(position, .1f);
+    }
+#endif
+    
+    void Update()
+    {
+        Ray rayz = Camera.main.ScreenPointToRay(Input.mousePosition);
+        position = rayz.GetPoint(rayDist);
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            cammera.Translate(rayz.direction * Time.deltaTime * speedMultiplier, Space.Self); 
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            cammera.Translate(-rayz.direction * Time.deltaTime * speedMultiplier, Space.Self);
+        }
+
+        pixel = Input.mousePosition;
+        
+
+        pixelLerp = Mathf.InverseLerp(-476, 1884, pixel.x);
+        Debug.Log(pixelLerp);
+
+        acPlane.rotation = Quaternion.Euler(Mathf.Lerp(-20, 20, pixelLerp),90,0);
+        //cammera.rotation = Quaternion.Euler(Mathf.Lerp(-20, 20, pixelLerp), 0, 0);
+        
+        //Change Transform Rotate (Based on Degrees)
+        
+    }
+
+    //Future Plans
+    // Plane should move in direction of vector position.
+    // "W" goes forward in Vector direction, "S" inverses the vector (go backwards)
+    // "A" and "D" can rotate the plane (and the camera) via Unit Circle to make smooth transitions
+    // Add Plane Model
+}
